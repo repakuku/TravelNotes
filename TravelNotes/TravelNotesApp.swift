d656043951c9c2efba75b0e8838f31ba90cebdb2
Alexey Turulin <repakuku@icloud.com>
2024-04-23 14:42:28 -0400

    added amplify

--- TravelNotes/TravelNotesApp.swift
+++ TravelNotes/TravelNotesApp.swift
@@ -5,10 +5,21 @@
 //  Created by Alexey Turulin on 4/23/24.
 //
 
+import Amplify
 import SwiftUI
 
 @main
 struct TravelNotesApp: App {
+
+	init() {
+		do {
+			try Amplify.configure()
+			print("initialized Amplify")
+		} catch {
+			print("Could not initialize Amplify: \(error)")
+		}
+	}
+
     var body: some Scene {
         WindowGroup {
             NotesView()
