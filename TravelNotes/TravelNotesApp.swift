2bfdd83b32621e454b53d74670cf4e3adf049e24
Alexey Turulin <repakuku@icloud.com>
2024-04-23 13:24:55 -0400

    Initial Commit

---
+++ TravelNotes/TravelNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNotesApp.swift
+//  TravelNotes
+//
+//  Created by Alexey Turulin on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
